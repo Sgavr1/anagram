2b02ee94e59911a71c9f2ababa0c54cfcbde9356
Oleksandr Havrylko <sanya.gavrilko60@gmail.com>
2023-11-16 18:20:08 +0200

    Anagram function v3

--- src/main/java/org/example/AnagramMaker.java
+++ src/main/java/org/example/AnagramMaker.java
@@ -1,40 +1,59 @@
 package org.example;
 
 public class AnagramMaker {
-    public String make(String word){
+    public String make(String text){
 
-        if (word == null || word.isEmpty()) {
+        if (text == null || text.isEmpty()) {
             return "";
         }
 
-        char[] chars = word.toCharArray();
+        String anagramLine = "";
 
-        int firstIndex = 0;
-        int lastIndex = chars.length - 1;
+        for (String word : text.split("\\s+")) {
 
-        while (firstIndex<lastIndex){
-            if (checkEnglishLetter(chars[firstIndex])){
-                if (checkEnglishLetter(chars[lastIndex])){
+            char[] chars = word.toCharArray();
 
-                    char swap = chars[firstIndex];
-                    chars[firstIndex] = chars[lastIndex];
-                    chars[lastIndex] = swap;
+            int firstIndex = 0;
+            int lastIndex = chars.length - 1;
+
+            while (firstIndex < lastIndex){
+
+                char firstChar = chars[firstIndex];
+                char lastChar = chars[lastIndex];
+
+                boolean firstIsLetter = isEnglishLetter(firstChar);
+                boolean lastIsLetter = isEnglishLetter(lastChar);
+
+                if (firstIsLetter && lastIsLetter){
+                    chars[firstIndex] = lastChar;
+                    chars[lastIndex] = firstChar;
 
                     firstIndex++;
                     lastIndex--;
 
-                } else {
+                } else if (firstIsLetter) {
+
                     lastIndex--;
+
+                } else if (lastIsLetter) {
+
+                    firstIndex++;
+
+                } else {
+
+                    firstIndex++;
+                    lastIndex--;
+
                 }
-            } else {
-                firstIndex++;
             }
+
+            anagramLine += new String(chars) + " ";
         }
 
-        return new String(chars);
+        return anagramLine.trim();
     }
 
-    private boolean checkEnglishLetter(char value){
+    private boolean isEnglishLetter(char value){
 
         if ((value > 64 && value < 91) || (value > 96 && value < 123)){
             return true;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,11 @@
 package org.example;
-
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
         String line  = new Scanner(System.in).nextLine();
-
         AnagramMaker anagramMaker = new AnagramMaker();
-
-        line = Arrays.stream(line.split("\\s+")).map(anagramMaker::make).collect(Collectors.joining(" "));
-
-        System.out.println(line);
+        System.out.println(anagramMaker.make(line));
     }
 }
